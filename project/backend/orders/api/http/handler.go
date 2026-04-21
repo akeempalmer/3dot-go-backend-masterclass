1fd83b2037217f38d3b79497ea9f4decc3099a03
Akeem Palmer <akeemk.palmer@gmail.com>
2026-04-20 23:33:45 -0400

    completed 03-http/01-http-handler

--- project/backend/orders/api/http/handler.go
+++ project/backend/orders/api/http/handler.go
@@ -2,8 +2,9 @@ package http
 
 import (
 	"context"
-
 	"eats/backend/common"
+
+	"github.com/google/uuid"
 )
 
 type Handler struct{}
@@ -13,5 +14,15 @@ func NewHandler() Handler {
 }
 
 func Register(ctx context.Context, e common.EchoRouter, handler Handler) error {
+	RegisterHandlers(e, NewStrictHandler(handler, nil))
+
 	return nil
+}
+
+func (h Handler) RegisterCustomer(ctx context.Context, request RegisterCustomerRequestObject) (RegisterCustomerResponseObject, error) {
+
+	customerUuid := uuid.New()
+	return RegisterCustomer201JSONResponse{
+		CustomerUuid: customerUuid,
+	}, nil
 }
