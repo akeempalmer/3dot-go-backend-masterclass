e0e84829eb5053c6c1d8898eca1c9b5dbdbb9dac
Three Dots Labs <contact@threedotslabs.com>
2026-05-02 21:53:27 -0400

    completed 08-advanced-repositories/01-currency-enum

---
+++ project/backend/common/shared/currency.go
@@ -0,0 +1,37 @@
+package shared
+
+import (
+	"fmt"
+
+	"eats/backend/common"
+)
+
+type Currency struct {
+	common.Enum[CurrencyType]
+}
+
+func (c Currency) Code() string {
+	return c.String()
+}
+
+type CurrencyType string
+
+func (c CurrencyType) Values() []string {
+	return []string{
+		"USD",
+		"EUR",
+		"GBP",
+		"JPY",
+		"PLN",
+	}
+}
+
+func MustNewCurrency(value string) Currency {
+	c := Currency{}
+	err := c.UnmarshalText([]byte(value))
+	if err != nil {
+		panic(fmt.Errorf("error unmarshalling currency value: %s", value))
+	}
+
+	return c
+}
